777e82c2e39c8c9d09ad30e4b08311ee872638e4
Kenmak77 <xelradark@gmail.com>
2025-10-18 16:35:27 +0200

    better

---
+++ P+FR_Netplay/fix_backend.ps1
@@ -0,0 +1,19 @@
+﻿$basePath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$dolphinIni = Join-Path $basePath "User\Config\Dolphin.ini"
+$selfPath = $MyInvocation.MyCommand.Path
+
+# --- Nettoyage du fichier Dolphin.ini ---
+if (Test-Path $dolphinIni) {
+    $content = Get-Content $dolphinIni -Raw
+
+    # Supprime entièrement la section [Analytics] et ses lignes
+    $content = $content -replace '(?ms)^\[Analytics\][^\[]*', ''
+
+    # Réécriture propre du fichier en UTF-8 sans BOM
+    $utf8 = New-Object System.Text.UTF8Encoding $false
+    [System.IO.File]::WriteAllText($dolphinIni, $content.Trim(), $utf8)
+}
+
+# --- Auto-suppression du script ---
+Start-Sleep -Seconds 1
+Remove-Item -Path $selfPath -Force
